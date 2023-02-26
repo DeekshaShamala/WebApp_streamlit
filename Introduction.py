aee14501a1ee264a75a3d8fc31b3df559debaf00
DeekshaShamala <111628109+DeekshaShamala@users.noreply.github.com>
2023-02-26 17:20:38 +0530

    Add files via upload

--- Introduction.py
+++ Introduction.py
@@ -4,7 +4,7 @@ st.subheader("Introduction:")
 
 st.write("Hello there, I am Deeksha Reddy S, a Data Science Intern at Innomatics Research Labs. This is the first web application that I have created using streamlit. The next page contains information about the diamond dataset that have I chosen for analysis and various visualization charts to understand the data in a better way.")
 
-st.subheader("Contact Me")
+st.subheader("Contact Me:")
 
 st.write("You may connect with me on any of the palforms below. I would love to connect with you.")
 
